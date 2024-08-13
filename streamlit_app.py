9a8eb7ac2fdd9341746958afbc3dce284662d5dc
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-13 19:22:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,6 @@
 import streamlit as st
 import numpy as np
-import pandas as pd
-import os.path
 import streamlit_gsheets
 from streamlit_gsheets import GSheetsConnection
-#from google.auth.transport.requests import Request
-#from google.oauth2.credentials import Credentials
-#from google_auth_oauthlib.flow import InstalledAppFlow
-#from googleapiclient.discovery import build
-#from googleapiclient.errors import HttpError
-
-# Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
-
 df = conn.read()
