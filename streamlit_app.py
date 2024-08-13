cb77a76263b2d0142062d1911f1f932193e9907e
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-13 19:28:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import gspread
 import streamlit_gsheets
 from streamlit_gsheets import GSheetsConnection
 conn = st.connection("gsheets", type=GSheetsConnection)
