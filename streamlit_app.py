d16730958fda61b162775d843d91ba8e096ca7b1
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-13 18:26:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import os.path
+import pandas as pd
 from streamlit_gsheets import GSheetsConnection
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+#from google.auth.transport.requests import Request
+#from google.oauth2.credentials import Credentials
 #from google_auth_oauthlib.flow import InstalledAppFlow
 #from googleapiclient.discovery import build
 #from googleapiclient.errors import HttpError
